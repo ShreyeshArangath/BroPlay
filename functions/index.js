8eeebca99dde2dc06ead8031601095a19165cf08
Shreyesh <imshreyesh17@gmail.com>
2020-08-30 01:49:27 -0500

    moved the user service api

--- functions/index.js
+++ functions/index.js
@@ -9,48 +9,8 @@ admin.initializeApp({
 })
 
 const db = admin.firestore();
-const userServiceAPI = express();
 const broPlayAPI = express();
-
-userServiceAPI.use(cors({ origin: true }));
-
-//GET: users in a broPlayRoom by roomId
-userServiceAPI.get("/:roomId", async (req, res) => {
-    const data = []
-    const roomId = req.params.roomId;
-    console.log(roomId);
-    const broPlayIdRef = db.collection('broPlayRoom').doc(roomId);
-    const snapshot = await broPlayIdRef.get()
-        .then((doc) => {
-            data.push(doc.data());
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    res.send((data));
-});
-
-userServiceAPI.post("/:roomId", async (req, res) => {
-    let preExistingUsers = []
-    const user = req.body;
-    const roomId = req.params.roomId;
-    const broPlayIdRef = db.collection('broPlayRoom').doc(roomId);
-    await broPlayIdRef.get()
-        .then((doc) => {
-            preExistingUsers.push(doc.data());
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-    preExistingUsers = preExistingUsers[0];
-    preExistingUsers.users.push(user.userName);
-
-    await broPlayIdRef.update(preExistingUsers);
-    res.send(preExistingUsers);
-})
-
-
+const userServiceAPI = require('./src/UserService');
 exports.broPlayRooms = functions.https.onRequest(userServiceAPI);
 
 // app.post("/users", async (req, res)=>{
